bb65953f9c21700c59b0d357c4ebb8a7e457b27c
Raj Gara <rgara@MacBook-JCX9XKQGPX.local>
2025-10-27 19:33:38 -0400

    always syncing bills issue fix

--- imports/api/publications.ts
+++ imports/api/publications.ts
@@ -3,7 +3,11 @@ import { Bills } from './bills';
 import { GlobalUsers } from './users';
 
 Meteor.publish('bills.all', function() {
-	return Bills.find({});
+	console.log('📡 bills.all publication called');
+	const cursor = Bills.find({});
+	const count = cursor.count();
+	console.log('📊 Publishing', count, 'bills');
+	return cursor;
 });
 
 Meteor.publish('globalUsers.all', function() {
